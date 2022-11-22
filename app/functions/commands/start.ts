060ad759c65cedf93a23a66f3d05f29b684a4221
marco.abate <marco.abate@scuolazoo.it>
2022-11-22 08:55:38 +0100

    cleaning start.ts

--- app/functions/commands/start.ts
+++ app/functions/commands/start.ts
@@ -6,16 +6,15 @@ import type { PlayerInterface } from "../../types/player.interfaces";
 
  const start = async (): Promise<void> => {
      bot.command("start", async (ctx) => {
+        
         const player: PlayerInterface = await db.player.get({
             id: "51104298",
         });
-        
-        console.log("PLAYER: ", player)
-        
-        //  if (users.id.toString() !== "0") {
-        //      await db.users.update({ id: users.id }, telegram.api.message.getFullUser(ctx));
+                
+        //  if (player.id.toString() !== "0") {
+        //      await db.player.update({ id: player.id }, telegram.api.message.getFullUser(ctx));
         //  } else {
-        //      await db.users.add(telegram.api.message.getFullUser(ctx));
+        //      await db.player.add(telegram.api.message.getFullUser(ctx));
         //  }
  
          if (ctx.msg.chat.id < 0) {
@@ -25,10 +24,6 @@ import type { PlayerInterface } from "../../types/player.interfaces";
             await ctx.reply("Start command private")
 
          }
-     });
-
-     bot.command("prova", async (ctx) => {
-            await ctx.reply("Fiet")
      });
  };
  
